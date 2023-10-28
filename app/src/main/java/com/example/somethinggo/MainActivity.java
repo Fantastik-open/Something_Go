07a0aec48585d97da1e3438fde3dfbf77cf12eac
lolesinskil24 <lolesinskil24@gmail.com>
2023-10-28 11:24:13 -0400

    Changed MainActivity.java to add sensor info.

--- app/src/main/java/com/example/somethinggo/MainActivity.java
+++ app/src/main/java/com/example/somethinggo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.somethinggo;
 
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -27,6 +30,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -41,4 +50,67 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
+
+    private final SensorEventListener sensorEventListener = new SensorEventListener() {
+        float[] accelOutput;
+        float[] magOutput;
+        float[] gyroOutput;
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            switch (event.sensor.getType()) {
+                case Sensor.TYPE_ACCELEROMETER:
+                    accelOutput = event.values;
+                    break;
+                case Sensor.TYPE_MAGNETIC_FIELD:
+                    magOutput = event.values;
+                    break;
+                case Sensor.TYPE_GYROSCOPE:
+                    gyroOutput = event.values; // Use this for movement detection
+                    break;
+            }
+
+            if (accelOutput != null && magOutput != null) {
+                float[] R = new float[9];
+                float[] I = new float[9];
+                boolean success = SensorManager.getRotationMatrix(R, I, accelOutput, magOutput);
+                if (success) {
+                    float[] orientation = new float[3];
+                    SensorManager.getOrientation(R, orientation);
+                    float azimuth = orientation[0]; // compass direction
+                    // Use the azimuth for your compass functionality
+                }
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // Handle accuracy changes, if necessary
+        }
+
+    };
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (accelerometer != null) {
+            sensorManager.registerListener(sensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        if (magnetometer != null) {
+            sensorManager.registerListener(sensorEventListener, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        if (gyroscope != null) {
+            sensorManager.registerListener(sensorEventListener, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(sensorEventListener);
+    }
 }
+
