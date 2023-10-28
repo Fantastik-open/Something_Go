939db93f00f2d8abcc3182d09c5f4c9985afbbce
lolesinskil24 <lolesinskil24@gmail.com>
2023-10-28 19:38:09 -0400

    Image should almost display to screen.

--- app/src/main/java/com/example/somethinggo/ui/camera/CameraFragment.java
+++ app/src/main/java/com/example/somethinggo/ui/camera/CameraFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,7 +25,10 @@ public class CameraFragment extends Fragment {
         binding = FragmentCameraBinding.inflate(inflater, container, false);
         final TextView textView = binding.textCamera;
         cameraViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         View root = binding.getRoot();
+
+        final ImageView cameraView = binding.CameraView;
 
         return root;
     }
