18ff569b5bb50787e1b6a56df39868098a905a96
lolesinskil24 <lolesinskil24@gmail.com>
2023-10-28 20:15:00 -0400

    Combining CameraFragment with MapImporterActivity.java

--- app/src/main/java/com/example/somethinggo/MapImporterActivity.java
+++ app/src/main/java/com/example/somethinggo/MapImporterActivity.java
@@ -1,86 +1,86 @@
-package com.example.somethinggo;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.activity.result.contract.ActivityResultContracts;
-
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
-
-
-public class MapImporterActivity extends AppCompatActivity {
-    private static final int REQUEST_CAMERA_PERMISSION = 1234;
-    private ImageView capturedImageView; // Declare the ImageView
-
-    // Define this as an instance variable
-    private final ActivityResultLauncher<Intent> mTakePictureLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            result -> {
-                if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
-                    Bundle extras = result.getData().getExtras();
-                    assert extras != null;
-                    Bitmap imageBitmap = (Bitmap) extras.get("data");
-
-                    // Set the image to the ImageView here
-                    capturedImageView.setImageBitmap(imageBitmap);
-                }
-            });
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map_importer);
-
-        capturedImageView = findViewById(R.id.captured_map2);
-
-        // Set the button's onClickListener
-        Button captureButton = findViewById(R.id.capture_image);
-        captureButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkCameraPermissionAndTakePicture();
-            }
-        });
-    }
-
-    private void checkCameraPermissionAndTakePicture() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
-        } else {
-            dispatchTakePictureIntent();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_CAMERA_PERMISSION) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                dispatchTakePictureIntent();
-            } else {
-                // Handle the case where the user denied the permission
-                Toast.makeText(this, "Camera permission is required for this functionality.", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    @SuppressLint("QueryPermissionsNeeded")
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            mTakePictureLauncher.launch(takePictureIntent);
-        }
-    }
-}
-
+//package com.example.somethinggo;
+//import androidx.activity.result.ActivityResultLauncher;
+//import androidx.annotation.NonNull;
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.core.app.ActivityCompat;
+//import androidx.core.content.ContextCompat;
+//import androidx.activity.result.contract.ActivityResultContracts;
+//
+//import android.Manifest;
+//import android.annotation.SuppressLint;
+//import android.app.Activity;
+//import android.content.Intent;
+//import android.content.pm.PackageManager;
+//import android.graphics.Bitmap;
+//import android.os.Bundle;
+//import android.provider.MediaStore;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.ImageView;
+//import android.widget.Toast;
+//
+//
+//public class MapImporterActivity extends AppCompatActivity {
+//    private static final int REQUEST_CAMERA_PERMISSION = 1234;
+//    private ImageView capturedImageView; // Declare the ImageView
+//
+//    // Define this as an instance variable
+//    private final ActivityResultLauncher<Intent> mTakePictureLauncher = registerForActivityResult(
+//            new ActivityResultContracts.StartActivityForResult(),
+//            result -> {
+//                if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+//                    Bundle extras = result.getData().getExtras();
+//                    assert extras != null;
+//                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+//
+//                    // Set the image to the ImageView here
+//                    capturedImageView.setImageBitmap(imageBitmap);
+//                }
+//            });
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_map_importer);
+//
+//        capturedImageView = findViewById(R.id.captured_map2);
+//
+//        // Set the button's onClickListener
+//        Button captureButton = findViewById(R.id.capture_image);
+//        captureButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                checkCameraPermissionAndTakePicture();
+//            }
+//        });
+//    }
+//
+//    private void checkCameraPermissionAndTakePicture() {
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+//        } else {
+//            dispatchTakePictureIntent();
+//        }
+//    }
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                dispatchTakePictureIntent();
+//            } else {
+//                // Handle the case where the user denied the permission
+//                Toast.makeText(this, "Camera permission is required for this functionality.", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
+//
+//    @SuppressLint("QueryPermissionsNeeded")
+//    private void dispatchTakePictureIntent() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            mTakePictureLauncher.launch(takePictureIntent);
+//        }
+//    }
+//}
+//
