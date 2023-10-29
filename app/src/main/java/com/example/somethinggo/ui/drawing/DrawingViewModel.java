43aedd93cc0e35dd0eb73da2792f17537c0718e3
Fantastik-open <fantastikbruh@gmail.com>
2023-10-29 05:42:56 -0400

    Fix Most Errors on Drawing Fragment

--- app/src/main/java/com/example/somethinggo/ui/drawing/DrawingViewModel.java
+++ app/src/main/java/com/example/somethinggo/ui/drawing/DrawingViewModel.java
@@ -1,16 +1,23 @@
 package com.example.somethinggo.ui.drawing;
 
+import android.graphics.Bitmap;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class DrawingViewModel extends ViewModel {
-
     private final MutableLiveData<String> mText;
+    private final MutableLiveData<Bitmap> imageBitmap;
 
     public DrawingViewModel() {
+        imageBitmap = new MutableLiveData<>();
         mText = new MutableLiveData<>();
         mText.setValue("This is drawing fragment");
+    }
+
+    public LiveData<Bitmap> getImage() {
+        return imageBitmap;
     }
 
     public LiveData<String> getText() {
