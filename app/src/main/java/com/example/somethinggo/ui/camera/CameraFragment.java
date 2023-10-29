fc1fa1c6ef62132de3a2ce1f5b06627e8b788415
lolesinskil24 <lolesinskil24@gmail.com>
2023-10-28 20:13:21 -0400

    Image should almost display to screen.

--- app/src/main/java/com/example/somethinggo/ui/camera/CameraFragment.java
+++ app/src/main/java/com/example/somethinggo/ui/camera/CameraFragment.java
@@ -1,13 +1,26 @@
 package com.example.somethinggo.ui.camera;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -16,21 +29,66 @@ import com.example.somethinggo.databinding.FragmentCameraBinding;
 public class CameraFragment extends Fragment {
 
     private FragmentCameraBinding binding;
+    private CameraViewModel cameraViewModel;
+    private static final int REQUEST_CAMERA_PERMISSION = 1234;
+
+    // Define this as an instance variable
+    private final ActivityResultLauncher<Intent> mTakePictureLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                    Bundle extras = result.getData().getExtras();
+                    assert extras != null;
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+                    // Set the image to the ImageView here
+                    binding.CameraView.setImageBitmap(imageBitmap);
+                }
+            });
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        CameraViewModel cameraViewModel =
-                new ViewModelProvider(this).get(CameraViewModel.class);
-
         binding = FragmentCameraBinding.inflate(inflater, container, false);
-        final TextView textView = binding.textCamera;
-        cameraViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
-        View root = binding.getRoot();
+        cameraViewModel = new ViewModelProvider(this).get(CameraViewModel.class);
+        cameraViewModel.getText().observe(getViewLifecycleOwner(), binding.textCamera::setText);
 
-        final ImageView cameraView = binding.CameraView;
+        binding.CameraView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkCameraPermissionAndTakePicture();
+            }
+        });
 
-        return root;
+        return binding.getRoot();
+    }
+
+    private void checkCameraPermissionAndTakePicture() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+        } else {
+            dispatchTakePictureIntent();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                dispatchTakePictureIntent();
+            } else {
+                Toast.makeText(requireContext(), "Camera permission is required for this functionality.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(requireActivity().getPackageManager()) != null) {
+            mTakePictureLauncher.launch(takePictureIntent);
+        }
     }
 
     @Override
--- app/src/main/java/com/example/somethinggo/ui/camera/CameraViewModel.java
+++ app/src/main/java/com/example/somethinggo/ui/camera/CameraViewModel.java
@@ -1,16 +1,23 @@
 package com.example.somethinggo.ui.camera;
 
+import android.graphics.Bitmap;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class CameraViewModel extends ViewModel {
-
     private final MutableLiveData<String> mText;
+    private final MutableLiveData<Bitmap> imageBitmap;
 
     public CameraViewModel() {
+        imageBitmap = new MutableLiveData<>();
         mText = new MutableLiveData<>();
         mText.setValue("This is camera fragment");
+    }
+
+    public LiveData<Bitmap> getImage() {
+        return imageBitmap;
     }
 
     public LiveData<String> getText() {
